9b026935ea42c6e9c27e4d75d77a04dd87b75794
taraleos45 <dorian.boitard@gmeil.com>
2022-08-29 23:04:15 +0200

    add new tipseur and request

--- 1steptotweepy.py
+++ 1steptotweepy.py
@@ -23,6 +23,11 @@ callback_url = 'oob' #https://cfe.sh/twitter.callback
 
 client = tweepy.Client(beared,return_type = requests.Response) #api_key,api_secrets,access_token,access_secret,
 
+if __name__ == "__main__":
+    # call your code here
+    #main()
+    
+    
 #auth = tweepy.OAuth1UserHandler(api_key,api_secrets,access_token,access_secret)
 #api = tweepy.API(auth)
 #redi = auth.get_authorization_url()
@@ -34,50 +39,52 @@ client = tweepy.Client(beared,return_type = requests.Response) #api_key,api_secr
 #new_status = api.update_status("Hello frome my python script")
 #re = client.get_liked_tweets('1104882393557712896',max_results=5)
 #client.retweet('1556267441650540551', user_auth=False)
-df = pd.read_excel ('tipseur_twitter_id.xlsx')
-print('numéro de requête : ')
-id_requete = int(input())
-#id_requete = 4 #0,1,2,4,5
-dernier_id = int(df['dernier_id_requete'][id_requete])
-
-requete = df['str_requette'][id_requete]
-nom_tip = df['Tipseur'][id_requete]
-file_name = str(nom_tip)+".json"
-#print(df['str_requette'])
-
-#search = client.search_recent_tweets('"zufygzeuygfuerzf" -is:reply -is:retweet lang:fr',max_results=10,tweet_fields=["public_metrics"])
-search = client.search_recent_tweets(requete,max_results=10,since_id=dernier_id) #since_id=dernier_id
-#print(search.json())
-search_json = search.json()
-if search_json['meta']['result_count'] != 0:
-    print('Data found '+ str(search_json['meta']['result_count'])+ " Tweets")
-    newest_id = search_json['meta']['newest_id']
-    df.at[id_requete,'dernier_id_requete'] = str(newest_id)
-    df['dernier_id_requete'] = df['dernier_id_requete'].astype(str)
-    df.to_excel("tipseur_twitter_id.xlsx", sheet_name="new",index=False)
-
-#g = open("test.txt","w",encoding="utf-8")
-    ff = open("json_file/"+file_name, "r",encoding="utf-8")
+    for ind in range(11):
+        df = pd.read_excel ('tipseur_twitter_id.xlsx')
+        id_requete = int(ind)
+        #id_requete = 4 #0,1,2,4,5
+        dernier_id = int(df['dernier_id_requete'][id_requete])
+        
+        requete = df['str_requette'][id_requete]
+        nom_tip = df['Tipseur'][id_requete]
+        file_name = str(nom_tip)+".json"
+        #print(df['str_requette'])
+        
+        #search = client.search_recent_tweets('"zufygzeuygfuerzf" -is:reply -is:retweet lang:fr',max_results=10,tweet_fields=["public_metrics"])
+        search = client.search_recent_tweets(requete,max_results=50,since_id=dernier_id) #since_id=dernier_id
+        #print(search.json())
+        search_json = search.json()
+        if search_json['meta']['result_count'] != 0:
+            print('Data found '+ str(search_json['meta']['result_count'])+ " Tweets pour " + nom_tip)
+            newest_id = search_json['meta']['newest_id']
+            df.at[id_requete,'dernier_id_requete'] = str(newest_id)
+            df['dernier_id_requete'] = df['dernier_id_requete'].astype(str)
+            df.to_excel("tipseur_twitter_id.xlsx", sheet_name="new",index=False)
+        
+        #g = open("test.txt","w",encoding="utf-8")
+            ff = open("json_file/"+file_name, "r",encoding="utf-8")
+            
+            if os.stat("json_file/"+file_name).st_size == 0:
+                g = open("json_file/"+file_name,"w",encoding="utf-8")
+                #print('empty file')
+                json.dump(search_json,g)
+            else:
+                #print('not empty file')
+                before_json = json.load(ff)
+                ff.close()
+                g = open("json_file/"+file_name,"w",encoding="utf-8")
+                json_conc = search_json['data']+before_json['data']
+                dic = {'data' : json_conc}
+                #g.write(str(dic))
+                json.dump(dic,g)
+            
+            g.close()   
+        else:
+            print('No data found pour ' + nom_tip)
     
-    if os.stat("json_file/"+file_name).st_size == 0:
-        g = open("json_file/"+file_name,"w",encoding="utf-8")
-        print('empty file')
-        json.dump(search_json,g)
-    else:
-        print('not empty file')
-        before_json = json.load(ff)
-        ff.close()
-        g = open("json_file/"+file_name,"w",encoding="utf-8")
-        json_conc = search_json['data']+before_json['data']
-        dic = {'data' : json_conc}
-        #g.write(str(dic))
-        json.dump(dic,g)
-    
-    g.close()   
-else:
-    print('No data found')
+print('data_import ended')
 
-print('data_imported')
+
 
 #◘g.write('tu veux voir ma bite')
 
